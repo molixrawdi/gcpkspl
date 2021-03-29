8f20897830ab995d61229501798248c8698f1399
Molix <molix@tw6.co>
2021-03-29 05:43:07 +0100

    Added the container based application component

--- gcpkspl-main.tf
+++ gcpkspl-main.tf
@@ -58,5 +58,56 @@ resource "google_container_cluster" "gcpkspl-cluster" {
   }
 }
 
+module "gce-container" {
+  source = "github.com/terraform-google-modules/terraform-google-container-vm"
+  # version = "0.1.0"
+
+  container = {
+    # image="gcr.io/google-samples/hello-app:1.0"
+    image="gcr.io/gcpkspl/pett-app:latest"
+    # env = [
+    #   {
+    #     name = "TEST_VAR"
+    #     value = "Hello World!"
+    #   }
+    # ],
+
+    # Declare volumes to be mounted.
+    # This is similar to how docker volumes are declared.
+    volumeMounts = [
+      {
+        mountPath = "/cache"
+        name      = "tempfs-0"
+        readOnly  = false
+      },
+      {
+        mountPath = "/persistent-data"
+        name      = "data-disk-0"
+        readOnly  = false
+      },
+    ]
+  }
+
+  # Declare the Volumes which will be used for mounting.
+  volumes = [
+    {
+      name = "tempfs-0"
+
+      emptyDir = {
+        medium = "Memory"
+      }
+    },
+    {
+      name = "data-disk-0"
+
+      gcePersistentDisk = {
+        pdName = "data-disk-0"
+        fsType = "ext4"
+      }
+    },
+  ]
+
+  restart_policy = "Always"
+}
 
 
